11193611fb94d90bc03007d1abdb69486c8e7dbc
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2021-04-11 16:50:06 -0400

    to_typedef_str

--- helpers/generate_function.py
+++ helpers/generate_function.py
@@ -2,9 +2,9 @@ from typing import Callable
 import Algorithmia
 
 """
-Converts a LaTex string into a Python function.
+Converts a LaTex string into a Python function string.
 """
-def generate_function(expression: str, debug=False) -> Callable[[int], str]:
+def to_pydef_str(expression):
 	input = expression
 	client = Algorithmia.client('sim7K6mfIVuU1NOVV6Kf+czVpDH1')
 	algo = client.algo('Jeffro/LatexLambda/1.1.2')
@@ -12,9 +12,15 @@ def generate_function(expression: str, debug=False) -> Callable[[int], str]:
 
 	str_def = algo.pipe(input).result['func']
 
+"""
+Converts a LaTex string into a Python function.
+"""
+def generate_function(expression: str, debug=False) -> Callable[[int], str]:
+	str_def = to_pydef_str(expression)
 	if debug:
 		print(str_def)
 
 	f = eval(str_def)
 
 	return f
+
